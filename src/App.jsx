bbc5b5a28d40d452c559d788ba81158df6a32d24
haihai00000003 <94513502@qq.com>
2019-11-25 15:55:11 +0800

    bug3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Routers from "@/router"
 import Tabs from "@/components/layout/tab"
+import ResetCsss from "@/assets/reset.js"
+
 class App extends React.Component{
   render(){
     return (
       <div>
+        <ResetCsss/>
         <Routers/>
         <Tabs/>
       </div>
